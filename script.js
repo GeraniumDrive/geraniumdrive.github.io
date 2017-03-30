2296f6bb299646282e82499647d725e64ab9bda7
John Crowe <thecrow1213@gmail.com>
2017-03-30 09:42:26 -0500

    More changes

--- script.js
+++ script.js
@@ -5,19 +5,10 @@ function resizeAndRepositionLogo() {
         windowHeight = window.innerHeight,
         windowWidth = window.innerWidth,
         newWidth = windowWidth/2
-    console.log(logo.size())
-        
-
-    // logo.attr('width', newWidth)
-    //     .attr('x', windowWidth/2 - newWidth/2)
-    //     .attr('y', windowHeight/2 - newHeight/2)
 
     logo.attr('width', newWidth)
         .attr('x', windowWidth/2 - newWidth/2)
         .attr('y', function(d, i) {
-            console.log(this)
-            console.log(d)
-            console.log(i)
             var dimensions = this.getBBox(),
                 ratio = dimensions.height / dimensions.width,
                 newHeight = ratio * newWidth
@@ -27,26 +18,15 @@ function resizeAndRepositionLogo() {
         })
 }
 if (d3.select(".logo").size() >= 1) {
-    // window.addEventListener('resize', resizeAndRepositionLogo)
+    window.addEventListener('resize', resizeAndRepositionLogo)
     resizeAndRepositionLogo()
 }
 
 function blobs() {
-    var blobContainer = d3.select('#blobs'),
-        windowHeight = window.innerHeight,
-        windowWidth = window.innerWidth
+    var path = d3.path()
 
-    blobContainer.attr('width', windowWidth).attr('height', windowHeight)
-
-    var blobs = d3.selectAll('.blob'),
-        xScale = d3.scaleLinear().domain([0, blobs.size()+1]).range([0, window.innerWidth])
-
-    // Set initial blob position
-    blobs.attr('transform', function(d, i) { return 'translate(' + xScale(i+1) + ',-100)' })
-
-    // Start Y axis transition
-    blobs.transition().duration(10000)
-        .attr('transform', function(d, i) { return 'translate(' + xScale(i+1) + ',' + windowHeight + ')' })
+    path.moveTo(100, 100)
+    path.arcTo(200, 200, 300, 300, 100)
+    path.closePath()
 }
-// window.addEventListener('resize', blobs)
 blobs()
