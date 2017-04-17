669a4381642a56438bc1559eb3148bb82caad78c
John Crowe <thecrow1213@gmail.com>
2017-04-17 16:58:48 -0500

    More updates, trying new stuff

--- script.js
+++ script.js
@@ -5,169 +5,27 @@ window.mobilecheck = function() {
     return check;
 }
 
-// document.ontouchmove = function(event){
-//     event.preventDefault();
+// function getLogoScaledDimensions() {
+//     var logoWrapper = d3.select("#wrapper"),
+//         logoDimensions = logoWrapper.node().getBBox(),
+//         logoAspectRatio = logoDimensions.height / logoDimensions.width,
+//         logoScale = window.mobilecheck() ? 2.5 : 3.5,
+//         logoWidth = window.innerWidth/logoScale,
+//         logoHeight = logoAspectRatio * logoWidth
+
+//     return {
+//         width: logoWidth,
+//         height: logoHeight,
+//         aspectRatio: logoAspectRatio
+//     }
 // }
 
-// function positionAndSize() {
+// function resize() {
+//     var logoWrapper = d3.select("#wrapper"),
+//         scaledDimensions = getLogoScaledDimensions()
 
-//     var logo = d3.selectAll(".logo")
-
-//     // Set view box for logo
-//     logo.attr('viewBox', function() {
-//         var dimensions = this.getBBox()
-//         return '0 0 ' + dimensions.width + ' ' + dimensions.height
-//     })
-
-//     logo.selectAll('path').attr('fill-opacity', 0).attr('stroke-opacity', 0)
-
-//     var logoBackground = d3.select('#logoBackground'),
-//         backgroundDimensions = logoBackground.node().getBBox(),
-//         backgroundRatio = backgroundDimensions.height / backgroundDimensions.width,
-//         backgroundWidth = window.innerWidth/2,
-//         backgroundHeight = backgroundRatio * (backgroundWidth)
-
-//     logoBackground.attr('width', backgroundWidth)
-//         .attr('height', backgroundHeight)
-//         .attr('x', window.innerWidth/2 - backgroundWidth/2)
-//         .attr('y', window.innerHeight/2 - backgroundHeight/2)
-
-//     var logoForefront = d3.select('#logoForefront'),
-//         forefrontDimensions = logoForefront.node().getBBox(),
-//         forefrontWidthOffset = backgroundDimensions.width - forefrontDimensions.width,
-//         forefrontHeightOffset = backgroundDimensions.height - forefrontDimensions.height,
-//         forefrontRatio = forefrontDimensions.height / forefrontDimensions.width,
-//         forefrontWidth = window.innerWidth/2 - forefrontWidthOffset,
-//         forefrontHeight = forefrontRatio * (forefrontWidth)
-    
-//     logoForefront.attr('width', forefrontWidth)
-//         .attr('height', forefrontHeight)
-//         .attr('x', window.innerWidth/2 - logoForefront.attr('width')/2)
-//         .attr('y', window.innerHeight/2 - logoForefront.attr('height')/2)
-
-//     var bottomBlob = d3.select('#botBlob'),
-//         topBlob = d3.select('#topBlob')
-        
-//     bottomBlob.attr('transform', function() {
-//         var dimensions = this.getBBox(),
-//             scaleWidthRatio = window.innerWidth / dimensions.width
-//             xPosDifference = (0 - dimensions.x)/scaleWidthRatio,
-//             yPosDistance = window.innerHeight - dimensions.y - (dimensions.height/1.5)
-
-//         return 'scale(' + scaleWidthRatio + ',1) ' +
-//             'translate(' + xPosDifference + ',' + yPosDistance + ')'
-//     })
-
-//     topBlob.attr('transform', function() {
-//         var dimensions = this.getBBox(),
-//             scaleWidthRatio = window.innerWidth / dimensions.width
-//             xPosDifference = (0 - dimensions.x)/scaleWidthRatio,
-//             yPosDistance = 0 - dimensions.y - (dimensions.height/3)
-
-//         return 'scale(' + scaleWidthRatio + ',1) ' +
-//             'translate(' + xPosDifference + ',' + yPosDistance + ')'
-//     })
-
-//     logo.selectAll('path').transition().duration(1000).attr('fill-opacity', 1).attr('stroke-opacity', 1)
+//     logoWrapper.attr('width', scaledDimensions.width)
 // }
-
-// function blobInitialPosition() {
-//     var midpoint = window.innerWidth/2, deviation = midpoint/2,
-//         dimensions = this.getBBox(),
-//         xPos = d3.randomUniform(midpoint - deviation, midpoint + deviation - dimensions.width)(),
-//         topOrBottom = Math.random() >= 0.5,
-//         topPos = -1 * dimensions.height,
-//         bottomPos = window.innerHeight + dimensions.height
-
-//     return 'translate(' + xPos + ',' + (topOrBottom ? topPos : bottomPos) + ')'
-// }
-
-// function blobTransform() {
-//     var currentValue = d3.select(this).attr('transform'),
-//         xTranslate = parseInt(currentValue.slice(currentValue.indexOf('(') + 1, currentValue.indexOf(','))),
-//         yTranslate = parseInt(currentValue.slice(currentValue.indexOf(',') + 1, currentValue.indexOf(')'))),
-//         dimensions = this.getBBox(),
-//         xPos = dimensions.x + xTranslate, yPos = dimensions.y + yTranslate,
-//         newYPos = yPos >= 0 ? (0 - dimensions.height) : (window.innerHeight + dimensions.height)
-    
-//     return 'translate(' + xPos + ',' + newYPos + ')'
-// }
-
-// function initializeBlobs() {
-//     // Depending on some number specified... copy paths of existing blobs and duplicate them
-//     var blobsContainer = d3.select('#blobs'),
-//         blobTemplates = blobsContainer.selectAll('path'),
-//         blobMultiplier = 10
-
-//     blobTemplates.each(function duplicate() {
-//         var currentBlob = d3.select(this),
-//             path = currentBlob.attr('d'),
-//             style = currentBlob.attr('style')
-
-//         for(var dup = 1; dup < blobMultiplier; dup++) {
-//             blobsContainer
-//                 .append('path')
-//                 .attr('d', path)
-//                 .attr('style', style)
-//         }
-//     })
-
-//     d3.select('#blobs').selectAll('path').attr('transform', blobInitialPosition)
-// }
-
-// function moveBlobs() {
-
-//     var allBlobs = d3.select('#blobs').selectAll('path')
-//     allBlobs.interrupt() // disable any pending transitions
-//     allBlobs
-//         .attr('transform', blobInitialPosition)
-//         .transition() // Transition up/down
-//         .duration(function() {
-//             // TODO maybe base speed off of the size of the blob?
-//             return d3.randomUniform(9000, 90000)()
-//         })
-//         .ease(d3.easeLinear)
-//         .on('start', function repeat() { //TODO cancel transitions on resize, and restart
-//             // Begin transform on start
-//             d3.active(this)
-//                 .attr('transform', blobTransform)
-//                 .transition() // now reverse!
-//                 .attr('transform', blobTransform)
-//                 .transition()
-//                 .on('start', repeat) // repeat!
-//         })// }
-
-// function enableFilter() {
-//     d3.select('#filterGroup').attr('filter', 'url(#goo)')
-// }
-
-
-// positionAndSize()
-// initializeBlobs()
-// moveBlobs()
-// window.addEventListener('resize', positionAndSize)
-// window.addEventListener('resize', moveBlobs)
-
-// if (!window.mobilecheck()) {
-//     enableFilter()
-// }
-
-function setLogoViewBox() {
-    var logoWrapper = d3.select("#wrapper"),
-        logoDimensions = logoWrapper.node().getBBox()
-
-    logoWrapper.attr('viewBox', '0 0 ' + logoDimensions.width + ' ' + logoDimensions.height)
-}
-
-function resize() {
-    var logoWrapper = d3.select("#wrapper"),
-        logoDimensions = logoWrapper.node().getBBox(),
-        logoAspectRatio = logoDimensions.height / logoDimensions.width,
-        logoWidth = window.innerWidth/2.5,
-        logoHeight = logoAspectRatio * logoWidth
-
-    logoWrapper.attr('width', logoWidth).attr('height', logoHeight)
-}
 
 function randomBetween(min,max)
 {
@@ -175,34 +33,22 @@ function randomBetween(min,max)
 }
 
 function animate() {
-    var tl = new TimelineMax(),
-        logoWrapper = d3.select("#wrapper"),
+    var logoWrapper = d3.select("#wrapper"),
         logoDimensions = logoWrapper.node().getBBox(),
-        logoAspectRatio = logoDimensions.height / logoDimensions.width,
-        logoWidth = window.innerWidth/2.5,
-        logoHeight = logoAspectRatio * logoWidth
+        blobs = document.querySelectorAll('.blob')
 
-    var blobs = document.querySelectorAll('#lava path')
-    for(var i = 0; i < blobs.length; i++){
-
-      var t = TweenMax.to(blobs[i], randomBetween(14, 50), {
-        y:logoHeight+100,
+    for(var i = 0; i < blobs.length; i++) {
+      var currentBlob = d3.select(blobs[i]).node().getBBox(),
+        speed = (currentBlob.width + currentBlob.height)/randomBetween(3, 4) +
+            randomBetween(1, currentBlob.width)
+      
+      TweenMax.to(blobs[i], /*randomBetween(5, 55)*/speed, {
+        y:logoDimensions.height*-1,
         repeat:-1,
-        repeatDelay:randomBetween(1, 3),
         yoyo:true,
         ease:Linear.easeNone
       })
-
-      tl.add(t, (i+1)/0.6)
     }
-
-    tl.seek(100)
 }
 
-setLogoViewBox()
-resize()
 animate()
-
-if (!window.mobilecheck()) {
-    window.addEventListener('resize', resize)
-}
